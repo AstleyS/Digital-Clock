97aeb1a21ebf6fcc735319f89712d2d4975067e6
Astley Santos <astley_394@hotmail.coml>
2021-02-02 16:05:28 +0100

    Finished a simple clock... Next, play with stylization and raising the bar a bit

--- js/app.js
+++ js/app.js
@@ -1,6 +1,9 @@
 // This functions is responsible for showing the time
 function showTime() {
 
+    // Getting clock element
+    const clock = document.getElementById('clock_display');
+    
     // Getting the date
     let date = new Date();
     
@@ -8,13 +11,16 @@ function showTime() {
     let min = date.getMinutes(); // 0-59
     let sec = date.getSeconds(); // 0-59
 
+    if (hour < 10) hour = "0" + hour;
+    if (min < 10) min = "0" + min;
+    if (sec < 10) sec = "0" + sec;
+
     let time = `${hour}:${min}:${sec}s`;
 
-    document.getElementById('clock-display').innerText = time
+    clock.innerText = time
 
-    
+    // Runs the function showTime every second
+    setTimeout(showTime, 1000);
 }
 
 showTime()
-// Runs the function showTime every second
-setInterval(showTime, 1000);
